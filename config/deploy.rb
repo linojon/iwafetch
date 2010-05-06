d1c7c21b8add07defa16ff9b78952a39a78ca569
Jonathan Linowes <linojon@gmail.com>
2010-05-06 18:06:12 -0400

    for deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,12 +1,9 @@
 set :application, "iwafetch"
-set :domain, "rails.parkerhill.com"
+set :domain, "parkerhill.railsplayground.net"
 set :user, "parkerhi"
 
-set :repository,  "#{user}@#{domain}:/home/#{user}/git/#{application}"
-#set :repository,  "/home/#{user}/git/#{application}"
+set :repository,  "git://github.com/linoj/iwafetch.git"
 set :scm, :git
-set :scm_username, user
-set :runner, user
 set :use_sudo, false
 set :branch, "master"
 set :deploy_via, :checkout
@@ -17,4 +14,15 @@ default_run_options[:pty] = true
 role :web, domain
 role :app, domain
 role :db, domain, :primary => true
+
+# set this for target deploy
+# (When fcgi, Remember to edit environment.rb for 'production')
+set :deploy_for, 'passenger'
+#set :deploy_for, 'fcgi'
+#set :deploy_for, 'mongrel'
+set :public_html, 'shared'
+#set :public_html, 'single'
+
+#set :database, 'sqlite3'
+set :database, 'mysql'
 
